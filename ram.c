c87e2e7e2261b155d3f92f8885e9e8e3b7b5d974
Jose <josemago3@hotmail.com>
2020-09-01 17:16:03 -0500

    Modificación del esqueleto

--- ram.c
+++ ram.c
@@ -7,7 +7,7 @@
   Sistemas Operativos 2*/
 //estrucutura para listas ligadas
 
-int ram[79], mapabits[7][79], recorrido[79], hdd[79];
+int ram[79], mapabits[10][8], recorrido[80], hdd[80];
 
 typedef struct nodo{
 	char nombreproceso[10];
@@ -20,14 +20,14 @@ void menu(){
 	
 }
 void menuA(){
-	int f=0, op, i=0 ;
+	int op, i=0 ;
 	do{
 		printf("Menu\n");
 		printf("1.Crear proceso\n");
 		printf("2. Eliminar proceso\n");
 		printf("3. Visualizar RAM\n");
-		printf("4. Intercambio(Aun no programar)\n");
-		printf("4. Salir\n");
+		printf("4. Representación RAM disponible\n");
+		printf("5. Salir\n");
 		scanf("%d",&op);
 		switch(op){
 			case 1: printf("Creando el proceso %d \n", i); //piensa si mejor int o char //scanf("%s", &);
@@ -36,14 +36,80 @@ void menuA(){
 			case 2:
 					break;
 			case 3:
+					vismem();
 					break;
 			case 4:
+					printf("Menu\n");
+					printf("1 Mapa de Bits");
+					printf("2 Lista libres");
+					scanf("%d",&op);
+					switch(op){
+						case 1:
+								mapits();//Llama a función mapa de beats
+								break;
+						case 2: 
+								lises();//Llama a función Lista Libres
+								break;		
+					}
 					break;
+			case 5: 
+					break;		
 		}
-	}while(op!=4);
+	}while(op!=5);
 	return;
 }
 void menuB(){
+	int op, i=0 ;
+	do{
+		printf("Menu\n");
+		printf("1.Crear proceso\n");
+		printf("2. Eliminar proceso\n");
+		printf("3. Visualizar RAM\n");
+		printf("4. Representación RAM disponible\n");
+		printf("5. Salir\n");
+		scanf("%d",&op);
+		switch(op){
+			case 1: printf("Creando el proceso %d \n", i); //piensa si mejor int o char //scanf("%s", &);
+					i++;
+					break;
+			case 2:
+					break;
+			case 3:	
+					vismem();//Llama la funcion Visualizar memoria
+					break;
+			case 4:
+					printf("Menu\n");
+					printf("1 Mapa de Bits");
+					printf("2 Lista libres");
+					scanf("%d",&op);
+					switch(op){
+						case 1:
+								mapits();//Llama a función mapa de beats
+								break;
+						case 2: 
+								lises();//Llama a función Lista Libres
+								break;		
+					}
+					break;
+			case 5: 
+					break;
+			case 6:
+					break;				
+		}
+	}while(op!=6);
+	return;
+}
+
+
+void vismem(){
+
+}
+
+void mapits(){
+
+}
+
+void lises(){
 
 }
 
