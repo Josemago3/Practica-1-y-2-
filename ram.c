87363237975821984129ca7d919a3b2f53c1b2fe
Jose <josemago3@hotmail.com>
2020-09-01 21:21:26 -0500

    Se agregaron opciones a los menús y los default en los switch

--- ram.c
+++ ram.c
@@ -4,23 +4,52 @@
 /*programa que simula el funcionamiento de la memoria RAM
   Ortega Zitle Ariel
   Aguirre Olvera Jose Manuel
-  Sistemas Operativos 2*/
+  Sistemas Operativos 2
+  
+  Lo que hace el programa es simular la memoria ram.
+  Apartir de dos opciones donde la opción A) Solo tiene 4 opciones para el manejo de la ram y 
+  La opcion B) tiene 5 opciones, la opcion agregada a B) es la opcion de enviar archivos al disco
+  a partir de la memoria RAM y de la RAM al disco.
+  
+  */
 //estrucutura para listas ligadas
 
-int ram[79], mapabits[10][8], recorrido[80], hdd[80];
+int ram[79], mapabits[10][8], recorrido[80], hdd[80], i=0;
 
 typedef struct nodo{
 	char nombreproceso[10];
 	int empieza_tam;
+	int tam;
 	int termina_tam;
 	struct nodo *sig;		
 	}nodo;
 
+
+
+
+
 void menu(){
-	
+	int op;
+	do{
+		printf("Menu\n");
+		printf("1.Crear proceso\n");
+		printf("2. Eliminar proceso\n");
+		printf("3. Salir");
+		scanf("%d",&op);
+		switch(op){
+			case 1: menuA();
+					break;
+			case 2: menuB();
+					break;
+			case 3: break;		
+			default: break;				
+		}
+	}while(op!=3);
+	return;
 }
+
 void menuA(){
-	int op, i=0 ;
+	int op, tam;
 	do{
 		printf("Menu\n");
 		printf("1.Crear proceso\n");
@@ -29,9 +58,13 @@ void menuA(){
 		printf("4. Salir\n");
 		scanf("%d",&op);
 		switch(op){
-			case 1: printf("Creando el proceso %d \n", i); //piensa si mejor int o char //scanf("%s", &);
+			case 1: 
+					printf("Ingrese el tamaño del proceso: ");
+					scanf("%d",&tam);
+					printf("\n");
+					printf("Creando el proceso %d \n", i); //piensa si mejor int o char //scanf("%s", &);
 					i++;
-					break;//<--- es este  ^
+					break;
 			case 2:
 					break;
 			case 3:
@@ -48,21 +81,14 @@ void menuA(){
 								break;		
 					}
 					break;
-			case 4: 
-					break;		
+			case 4: break;	
+			default: break;
+
 		}
 	}while(op!=4);
 	return;
 }
 
-
-// Sabes como se tiene que visualizar la memoria??? 
-
-/*	Pero en teams esa opción es la 5 
-
-Va entonces deja y quita una opcion
-
-*/
 void menuB(){
 	int op, i=0 ;
 	do{
@@ -96,7 +122,8 @@ void menuB(){
 			case 4: 
 					break;
 			case 5:
-					break;				
+					break;	
+			default: break;					
 		}
 	}while(op!=5);
 	return;
@@ -111,9 +138,8 @@ void lises(){
 }
 
 int main(int argc, char const *argv[])
-{
-		
-
+{		
+	menu();
 	return 0;
 }
 
